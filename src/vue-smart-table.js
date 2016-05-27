d55101439fd22aa5fe72ae5099514b113735caf3
Andrea <gurghet+c9@gmail.com>
2016-05-27 09:16:47 +0000

    Example Component added to built-ins

--- src/vue-smart-table.js
+++ src/vue-smart-table.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import SmartTable from './components/SmartTable'
+import ExampleComponent from './components/ExampleComponent'
 
 Vue.use(VueResource)
 Vue.component('smart-table', SmartTable)
 
 window.SmartTable = SmartTable
+window.ExampleComponent = ExampleComponent
