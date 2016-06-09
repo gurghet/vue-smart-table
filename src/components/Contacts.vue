f326b0c300d82f52f0652a7f7159a9e3e534ec0e
Andrea <gurghet+c9@gmail.com>
2016-06-09 02:01:34 +0000

    New default style!

---
+++ src/components/Contacts.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <span class="label">tel.</span>
+    <span class="phone">{{value.phone}}</span>
+    <span class="label">cell.</span>
+    <span class="cell">{{value.cell}}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        value: {}
+      }
+    }
+  }
+</script>
+
+<style scoped>
+div {
+  width: 160px;
+  font-family: monospace;
+}
+.phone {
+  float: right;
+  clear: right;
+  display: block;
+}
+
+.cell {
+  float: right;
+  clear: right;
+  display: block;
+}
+
+.label {
+  float: left;
+  clear: left;
+  display: block;
+}
+</style>
